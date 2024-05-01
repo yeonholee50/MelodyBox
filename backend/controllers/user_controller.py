297c2214982181496a4680a5ff5a914eb366362e
Yeon Lee <82836242+yeonholee50@users.noreply.github.com>
2024-05-01 11:04:14 -0400

    Revert "Groundwork setup - also added CRUD operations"

--- backend/controllers/user_controller.py
+++ backend/controllers/user_controller.py
@@ -1,30 +1,11 @@
-from flask import jsonify, request
-from bson.json_util import dumps
-from . import mongo
-from .models.user import User
+from flask import jsonify
 
-users_collection = mongo.db.users
-
-@app.route('/users/<user_id>')
+# Example route for retrieving user information
 def get_user_info(user_id):
-    user = users_collection.find_one_or_404({'_id': ObjectId(user_id)})
-    return jsonify(User.from_mongo(user).to_dict())
-
-@app.route('/users', methods=['POST'])
-def create_user():
-    data = request.get_json()
-    user = User.from_dict(data)
-    result = users_collection.insert_one(user.to_dict())
-    return jsonify(str(result.inserted_id))
-
-@app.route('/users/<user_id>', methods=['PUT'])
-def update_user(user_id):
-    data = request.get_json()
-    updated_user = User.from_dict(data)
-    result = users_collection.update_one({'_id': ObjectId(user_id)}, {'$set': updated_user.to_dict()})
-    return jsonify(result.modified_count)
-
-@app.route('/users/<user_id>', methods=['DELETE'])
-def delete_user(user_id):
-    result = users_collection.delete_one({'_id': ObjectId(user_id)})
-    return jsonify(result.deleted_count)
+    # Replace this with actual logic to fetch user info from database
+    user_info = {
+        "id": user_id,
+        "username": "john_doe",
+        "email": "john@example.com"
+    }
+    return jsonify(user_info)
--- backend/models/user.py
+++ backend/models/user.py
@@ -1,27 +1,5 @@
-from bson.objectid import ObjectId
-
 class User:
-    def __init__(self, username, email):
+    def __init__(self, user_id, username, email):
+        self.id = user_id
         self.username = username
         self.email = email
-
-    def to_dict(self):
-        return {
-            "username": self.username,
-            "email": self.email
-        }
-
-    @staticmethod
-    def from_dict(user_dict):
-        return User(
-            username=user_dict.get('username'),
-            email=user_dict.get('email')
-        )
-
-    @staticmethod
-    def from_mongo(mongo_doc):
-        return User(
-            username=mongo_doc.get('username'),
-            email=mongo_doc.get('email')
-        )
-
