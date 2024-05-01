02c769f055538592e64bc7d16141682e5948c038
Yeon Lee <82836242+yeonholee50@users.noreply.github.com>
2024-05-01 11:04:14 -0400

    Revert "Groundwork setup - also added CRUD operations"

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,10 @@
 from flask import Flask
-from flask_pymongo import PyMongo
 
 app = Flask(__name__)
-app.config['MONGO_URI'] = 'mongodb://localhost:27017/melodybox'
-mongo = PyMongo(app)
+
+@app.route('/')
+def index():
+    return 'Welcome to MelodyBox!'
 
 if __name__ == "__main__":
     app.run(debug=True)
-
