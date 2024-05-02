90f9ce125d6f326b0f3f4ed638c15d1296b5a8c4
Yeon Lee <82836242+yeonholee50@users.noreply.github.com>
2024-05-02 09:21:10 -0400

    added 3 edge cases for playlist

--- tests/test_user.py
+++ tests/test_user.py
@@ -11,24 +11,19 @@ class TestUser(unittest.TestCase):
     def test_login(self, mock_find_one):
         mock_find_one.return_value = {'_id': 'test_user_id', 'email': 'test@test.com', 'password': 'test_password'}
         
-        response = self.app.post('/login', json={'email': 'test@test.com', 'password': 'test_password'})
-        
-        self.assertEqual(response.status_code, 200)
+        response_valid_credentials = self.app.post('/login', json={'email': 'test@test.com', 'password': 'test_password'})
+        self.assertEqual(response_valid_credentials.status_code, 200)
     
-    @patch('controllers.user_controller.users_collection.find_one')
-    def test_login_invalid_credentials(self, mock_find_one):
-        mock_find_one.return_value = None
-        
-        response = self.app.post('/login', json={'email': 'test@test.com', 'password': 'wrong_password'}) 
-        self.assertEqual(response.status_code, 401)
+        response_invalid_credentials = self.app.post('/login', json={'email': 'test@test.com', 'password': 'wrong_password'})
+        self.assertEqual(response_invalid_credentials.status_code, 401)
     
     @patch('controllers.user_controller.users_collection.delete_one')
     def test_logout(self, mock_delete_one):
         mock_delete_one.return_value = MagicMock(deleted_count=1)
         
         response = self.app.post('/logout')
-        
         self.assertEqual(response.status_code, 200)
 
 if __name__ == '__main__':
     unittest.main()
+
